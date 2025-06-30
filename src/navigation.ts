79cf0b4f554705c305693f626b47d3a3e6a5ce35
Peter Eckmann <petereckmann@Peters-MacBook-Air-4.local>
2025-06-30 05:27:19 -0700

    test

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,1 +1,9 @@
 import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  // fill in your header data here
+};
+
+export const footerData = {
+  // fill in your footer data here
+};
