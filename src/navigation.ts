7d560d98bf364eaf6557a8f7a6d69d5faeb08513
Peter Eckmann <petereckmann@Peters-MacBook-Air-4.local>
2025-06-30 21:18:38 -0700

    npm fixed

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,9 +1,12 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
-
 export const headerData = {
+  links: [],
+  actions: [],
 };
 
 export const footerData = {
+  links: [],
+  secondaryLinks: [],
+  socialLinks: [],
   footNote: `
     Founded by <a href="https://petereckmann1.github.io/" target="_blank" rel="noopener noreferrer" class="text-primary hover:underline">Peter Eckmann</a>, 
     <a href="https://gilson.cloud.ucsd.edu/" target="_blank" rel="noopener noreferrer" class="text-primary hover:underline">Michael K. Gilson</a>, 
